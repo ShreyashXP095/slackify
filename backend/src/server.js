9a1f7955b7b727eded9fde931c806606b80f8c76
Shreyash - Shukla <s.shreyashxp@gmail.com>
2025-10-07 20:24:55 +0530

    implemented the auth functionality and also handle the user creation and deletion from the db, connected clerk and mongodb through inngest

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,19 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import connectDB from './config/db.js';
+import {clerkMiddleware} from "@clerk/express";
+import { inngest, functions } from './config/inngest.js';
+import { serve } from "inngest/express";
+
+
 
 const app = express();
+app.use(express.json());
+
+app.use(clerkMiddleware()); // req.auth will tell the use is authenticated or not
+
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
 
@@ -10,8 +21,23 @@ app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
-connectDB().then(() => {
-  app.listen(ENV.PORT, () => {
-    console.log(`Server Started on port ${ENV.PORT}`);
-  });
-});
+
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    if(ENV.NODE_ENV !== 'production'){
+      app.listen(ENV.PORT, () => {
+        console.log(`Server is running on port ${ENV.PORT}`);
+      });
+    }
+  } catch (error) {
+    console.error('Error starting server:', error);
+    process.exit(1);
+  }
+};
+
+
+startServer();
+
+export default app;
